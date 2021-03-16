7284f451a01cdc6653a6fd002c17c8a2cac8ba4a
buihoanganhtuan <zzzcruelfatezzz@gmail.com>
2021-03-17 02:47:24 +0900

    robofriends init commit

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    const cardComponents = robots.map((user, i) => {
+        return <Card 
+                    key={i} 
+                    id={user.id} 
+                    name={user.name} 
+                    email={user.email}
+                />
+    });
+    return (
+    <div>
+        {cardComponents}
+    </div>
+    );
+}
+
+export default CardList;
